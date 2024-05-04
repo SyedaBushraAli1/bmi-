36adeeaf80bab152edb82c5f79c1eda4483905ef
SyedaBushraAli1 <168065302+SyedaBushraAli1@users.noreply.github.com>
2024-05-04 07:48:23 -0700

    Add files via upload

---
+++ input_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'input_page.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  InputPageState createState() => InputPageState();
+}
+class InputPageState extends State<InputPage> {
+  @override
+  Widget build (BuildContext  context){
+
+  }
+}
