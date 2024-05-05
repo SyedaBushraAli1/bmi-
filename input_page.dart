05869c0329f60c1dafa7259b6ad2db843dde0d6f
SyedaBushraAli1 <168065302+SyedaBushraAli1@users.noreply.github.com>
2024-05-05 01:28:16 -0700

    video 4 add the icon widget and extract classes
    in this video add the icon widgets and extract classes to short the code

--- input_page.dart
+++ input_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 import 'input_page.dart';
 
 class InputPage extends StatefulWidget {
@@ -9,19 +12,22 @@ class InputPage extends StatefulWidget {
 }
 class RepeatContainerCode extends StatelessWidget {
 
-  RepeatContainerCode({required this.colors});
+  RepeatContainerCode({required this.colors , this.cardwidget});
   final Color colors;
+  final Widget cardwidget;
+
+
   @override
   Widget build (BuildContext  context){
     return Container(
       margin: EdgeInsets.all(15.0),
+      child : cardwidget,
       decoration: BoxDecoration(
           color : Color(0xFF1D1E33)
           borderRadius: BorderRadius.circular(10.0),
-
     )
+    );
   }
-
 }
 class InputPageState extends State<InputPage> {
   @override
@@ -35,28 +41,49 @@ class InputPageState extends State<InputPage> {
         Expanded(child:Row(
          children: <Widget>[
            Expanded(child: new RepeatContainerCode(( colors : Color(blue),
-           )
+    ),
     )
+    )
+    ]
     )
            Expanded(child: new RepeatContainerCode(( colors : Color(0xFFIE33),
+             cardwidget : Column(),
+              MainAxisAlignment: MainAxisAlignment.center,
+              children:<Widgets>
+           Icon(
+             FontAwesomeicons.male,
+              size: 80.0,
+           SizedBox(
+    height:15.0,
+    Text(
+    Text('MALE', style : TextStyle),
+    FontSize:18.0 ,
+    color : Color(0xFF8D8E98)
     )
 
-           )
 
-      )
+
+
+
+    ),
+
+    )
+
+      );
 
 
     ],
-    ),),
+    )
+
         Expanded(child:Row(
          children: <Widget>[
           Expanded(child: new RepeatContainerCode( colors : Color(0xFFIE33),
 
-    ),
+    )
 
 
-    ],
-    ),),
+    ]
+    ),)
         Expanded(child: new RepeatContainerCode(( colors : Color(0xFFIE33),
 
     )
@@ -65,4 +92,30 @@ class InputPageState extends State<InputPage> {
     )
     ],
   }
+}
+class RepeatTextIconWidget extends StatelessWidget {
+  RepeatTextIconWidget({@required this.iconData , this.label}),
+  final IconData iconData;
+  final String label;
+  @override
+  Widget build (BuildContext  context){
+
+  }
+  Expanded(child: new RepeatTextIconWidget( colors : Color(0xFFIE33);
+      return  Column(),
+         MainAxisAlignment: MainAxisAlignment.center,
+         children:<Widgets>
+         Icon(
+          FontAwesomeicons.male,
+          size: 80.0,
+          SizedBox(
+           height:15.0,
+            Text(
+               Text('MALE', style : TextStyle),
+                FontSize:18.0 ,
+                color : Color(0xFF8D8E98),
+  )
+
+
+
 }
