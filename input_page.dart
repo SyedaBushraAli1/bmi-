10a199b2e4209b28a86b39bf0ef5e60061aa9d01
SyedaBushraAli1 <168065302+SyedaBushraAli1@users.noreply.github.com>
2024-05-05 08:23:01 -0700

    video 7 use of ternanary operatore
    in this video we use ternary operator to make code easy and short

--- input_page.dart
+++ input_page.dart
@@ -1,70 +1,143 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'input_page.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:bmi/container_file.dart';
+import 'package:bmi/icon_file.dart';
 
 class InputPage extends StatefulWidget {
   @override
   InputPageState createState() => InputPageState();
 }
+
+const activeColor = Color(0xFF1D1E33);
+const deActiveColor = Color(0xFF111328);
+
+enum Gender {
+  male,
+  female,
+}
+
 class InputPageState extends State<InputPage> {
+  Gender selectGender;
+
   @override
-  Widget build (BuildContext  context){
+  Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         title: Text('BMI CALCULATOR'),
-    ),
-      body: const Column(
-      children: <Widget> [
-        Expanded(child:Row(
-         children: <Widget>[
-          Expanded(child: Container(
-            margin: EdgeInsets.all(15.0),
-            decoration: BoxDecoration(
-            color : Color(0xFF1D1E33)
-            borderRadius: BorderRadius.circular(10.0),
-            children <widget>)[
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectGender = Gender.male;
+                      });
+                    },
+                    child: RepeatContainerCode(
+                      colors: selectGender == Gender.male ? activeColor : deActiveColor,
+                      cardwidget: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(
+                            FontAwesomeIcons.male,
+                            size: 80.0,
+                          ),
+                          SizedBox(
+                            height: 15.0,
+                          ),
+                          Text(
+                            'MALE',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              color: Color(0xFF8D8E98),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectGender = Gender.female;
+                      });
+                    },
+                    child: RepeatContainerCode(
+                      colors: selectGender == Gender.female ? activeColor : deActiveColor,
+                      cardwidget: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(
+                            FontAwesomeIcons.female,
+                            size: 80.0,
+                          ),
+                          SizedBox(
+                            height: 15.0,
+                          ),
+                          Text(
+                            'FEMALE',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              color: Color(0xFF8D8E98),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-          child: Expanded(child: Container(
-            margin: EdgeInsets.all(15.0),
-            decoration: BoxDecoration(
-            color : Color(0xFF1D1E33)
-            borderRadius: BorderRadius.circular(10.0),
-            children <widget>),)
-
+          Expanded(
+            child: RepeatContainerCode(
+              colors: Color(0xFF1D1E33),
+              cardwidget: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    FontAwesomeIcons.male,
+                    size: 80.0,
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  Text(
+                    'MALE',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Color(0xFF8D8E98),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
-
-    ],
-    ),),
-        Expanded(child:Row(
-    children: <Widget>[
-          Expanded(child: Container(
-           margin: EdgeInsets.all(15.0),
-           decoration: BoxDecoration(
-           color : Color(0xFF1D1E33)
-           borderRadius: BorderRadius.circular(10.0),
-           children <widget>)[
-    ),
-          Expanded(child: Container(
-           margin: EdgeInsets.all(15.0),
-           decoration: BoxDecoration(
-           color : Color(0xFF1D1E33)
-           borderRadius: BorderRadius.circular(10.0),
-           children <widget>)[
-    ),
-
-    ],
-    ),),
-        Expanded(child: Container(
-         margin: EdgeInsets.all(15.0),
-         decoration: BoxDecoration(
-         color : Color(0xFF1D1E33)
-         borderRadius: BorderRadius.circular(10.0),
-         children <widget>)[
-    ) ,
-    ],
-    ],
-    )
-    ],
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: RepeatContainerCode(
+                    colors: Color(0xFF1D1E33),
+                  ),
+                ),
+                Expanded(
+                  child: RepeatContainerCode(
+                    colors: Color(0xFF1D1E33),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
