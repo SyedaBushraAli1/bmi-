f7f5705eac9a483e323ffe73300319febd0e25ba
SyedaBushraAli1 <168065302+SyedaBushraAli1@users.noreply.github.com>
2024-05-05 06:01:02 -0700

    Video6 Add the gestures in icon
    in this video we added gestures activation or deactivation in icons and set colors of icon

--- input_page.dart
+++ input_page.dart
@@ -1,33 +1,11 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
 import 'input_page.dart';
 
 class InputPage extends StatefulWidget {
   @override
   InputPageState createState() => InputPageState();
-}
-class RepeatContainerCode extends StatelessWidget {
-
-  RepeatContainerCode({required this.colors , this.cardwidget});
-  final Color colors;
-  final Widget cardwidget;
-
-
-  @override
-  Widget build (BuildContext  context){
-    return Container(
-      margin: EdgeInsets.all(15.0),
-      child : cardwidget,
-      decoration: BoxDecoration(
-          color : Color(0xFF1D1E33)
-          borderRadius: BorderRadius.circular(10.0),
-    )
-    );
-  }
 }
 class InputPageState extends State<InputPage> {
   @override
@@ -40,82 +18,53 @@ class InputPageState extends State<InputPage> {
       children: <Widget> [
         Expanded(child:Row(
          children: <Widget>[
-           Expanded(child: new RepeatContainerCode(( colors : Color(blue),
-    ),
-    )
-    )
-    ]
-    )
-           Expanded(child: new RepeatContainerCode(( colors : Color(0xFFIE33),
-             cardwidget : Column(),
-              MainAxisAlignment: MainAxisAlignment.center,
-              children:<Widgets>
-           Icon(
-             FontAwesomeicons.male,
-              size: 80.0,
-           SizedBox(
-    height:15.0,
-    Text(
-    Text('MALE', style : TextStyle),
-    FontSize:18.0 ,
-    color : Color(0xFF8D8E98)
-    )
+          Expanded(child: Container(
+            margin: EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+            color : Color(0xFF1D1E33)
+            borderRadius: BorderRadius.circular(10.0),
+            children <widget>)[
+          ),
+          child: Expanded(child: Container(
+            margin: EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+            color : Color(0xFF1D1E33)
+            borderRadius: BorderRadius.circular(10.0),
+            children <widget>),)
 
-
-
-
-
-    ),
-
-    )
-
-      );
-
+          ),
 
     ],
-    )
-
+    ),),
         Expanded(child:Row(
-         children: <Widget>[
-          Expanded(child: new RepeatContainerCode( colors : Color(0xFFIE33),
+    children: <Widget>[
+          Expanded(child: Container(
+           margin: EdgeInsets.all(15.0),
+           decoration: BoxDecoration(
+           color : Color(0xFF1D1E33)
+           borderRadius: BorderRadius.circular(10.0),
+           children <widget>)[
+    ),
+          Expanded(child: Container(
+           margin: EdgeInsets.all(15.0),
+           decoration: BoxDecoration(
+           color : Color(0xFF1D1E33)
+           borderRadius: BorderRadius.circular(10.0),
+           children <widget>)[
+    ),
 
-    )
-
-
-    ]
-    ),)
-        Expanded(child: new RepeatContainerCode(( colors : Color(0xFFIE33),
-
-    )
+    ],
+    ),),
+        Expanded(child: Container(
+         margin: EdgeInsets.all(15.0),
+         decoration: BoxDecoration(
+         color : Color(0xFF1D1E33)
+         borderRadius: BorderRadius.circular(10.0),
+         children <widget>)[
+    ) ,
     ],
     ],
     )
     ],
   }
-}
-class RepeatTextIconWidget extends StatelessWidget {
-  RepeatTextIconWidget({@required this.iconData , this.label}),
-  final IconData iconData;
-  final String label;
-  @override
-  Widget build (BuildContext  context){
-
-  }
-  Expanded(child: new RepeatTextIconWidget( colors : Color(0xFFIE33);
-      return  Column(),
-         MainAxisAlignment: MainAxisAlignment.center,
-         children:<Widgets>
-         Icon(
-          FontAwesomeicons.male,
-          size: 80.0,
-          SizedBox(
-           height:15.0,
-            Text(
-               Text('MALE', style : TextStyle),
-                FontSize:18.0 ,
-                color : Color(0xFF8D8E98),
-  )
-
-
-
 }
