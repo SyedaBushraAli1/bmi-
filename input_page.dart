ffc82bfde0080b2e68f72ae709efb8859ab8f9a2
SyedaBushraAli1 <168065302+SyedaBushraAli1@users.noreply.github.com>
2024-05-12 01:18:45 -0700

    video 12 (last weight and age widget)
    in this video set the weight and age widget and add the conatiner

--- input_page.dart
+++ input_page.dart
@@ -1,4 +1,6 @@
 import 'package:bmi/constant file.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:bmi/container_file.dart';
@@ -18,6 +20,7 @@ class InputPageState extends State<InputPage> {
   Gender selectGender;
   int sliderHeight = 180;
   int sliderWeight = 60;
+  int sliderAge=20;
 
   @override
   Widget build(BuildContext context) {
@@ -145,7 +148,20 @@ class InputPageState extends State<InputPage> {
                         ),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[],
+                          children: <Widget>[
+                            RoundIcon(
+                              iconData:FontAwesomeIcons.minus,
+                              onPress:  (){
+                                setState(() {
+                                  sliderWeight--;
+
+                                });
+                              },
+
+                              SizedBox(width: 10.0,)
+                            ),
+                            RoundIcon(),
+                          ],
                         ),
                       ],
                     ),
@@ -154,13 +170,79 @@ class InputPageState extends State<InputPage> {
                 Expanded(
                   child: RepeatContainerCode(
                     colors: Color(0xFF1D1E33),
+                    cardwidget: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'AGE',
+                          style: kLabelStyle,
+                        ),
+                        Text(
+                          sliderAge.toString(),
+                          style: kNumberStyle,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            RoundIcon(
+                                iconData:FontAwesomeIcons.minus,
+                                onPress:  (){
+                                  setState(() {
+                                    sliderAge--;
+
+                                  });
+                                },
+
+                                SizedBox(width: 10.0,)
+                            ),
+                            RoundIcon(
+                              iconData: FontAwesomeIcons.plus,
+                              onPress: () {
+                                setState(() {
+                                  sliderAge++;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
             ),
           ),
+          Container(
+            color: Color(0xFFEB1555),
+            margin: EdgeInsets.only(top: 10.0),
+            height: 80.0,
+            width: double.infinity,
+          )
         ],
       ),
+    );
+  }
+}
+
+
+class RoundIcon extends StatelessWidget {
+   RoundIcon({@required this.iconData ,@required this.onPress});
+   final IconData iconData;
+   final Function onPress;
+
+  @override
+  Widget build(BuildContext context {
+    return RawMaterialButton(
+      child: Icon(iconData),
+      onPressed: onPress,
+      elevation: 6.0,
+      constraints: ,BoxConstraints.tightFor(),
+      height=56.0,
+      weight=56.0,
+    ),
+   )
+    shape: Circle.Border(),
+    fillColor: Color(0xFF4C4F5E),
     );
   }
 }
