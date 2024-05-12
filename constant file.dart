167eb0e4e1a5d76f5a904fe51f5d2fa15515887b
SyedaBushraAli1 <168065302+SyedaBushraAli1@users.noreply.github.com>
2024-05-12 02:09:41 -0700

    video14 (design the result screen)
    in this video design the result screen

--- constant file.dart
+++ constant file.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 
-const KLabelStyle=style: TextStyle(
-fontSize: 18.0,
-color: Color(0xFF8D8E98),
-);
-const activeColor = Color(0xFF1D1E33);
+const activeColor = Color(0xFF1F1E33);
 const deActiveColor = Color(0xFF111328);
-const kNumberStyle= TextStyle( Fontsize:50.0 , fontWeight:fontWeight.w900,)
 
-const kLargeButtonStyle= TextStyle (
-fontSize: 25.0,
-fontWeight:fontWeight.bold,
+const kLabelStyle = TextStyle(
+  fontSize: 20.0,
+  color: Color(0xFF8D8E98),
+);
+
+const kNumberStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+
+const kLargeButtonstyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kTittleStyle2 = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
 );
